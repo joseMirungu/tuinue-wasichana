1d24c24908f7703755814b93aae2e5ccabfc2de1
joseMirungu <josephirungu890@gmail.com>
2025-02-06 21:18:17 +0300

    file commit

--- backend/config.py
+++ backend/config.py
@@ -2,11 +2,11 @@ import os
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-secret-key')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
-    CORS_ORIGINS = ["https://tuinue-wasichana.vercel.app"]
+    CORS_ORIGINS = ["https://tuinue-wasichana-j25zb6jip-josemirungus-projects.vercel.app"]
